3d6098e35d0fdb6a7c533dbc358f8576c3abdcd3
Martin Terziev <m.terziev08@abv.bg>
2025-12-28 18:12:02 +0200

    Add project files.

---
+++ Discord Time Tracker/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Discord_Time_Tracker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
